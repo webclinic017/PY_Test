ff000c7d377b581d11852be6268079f79f2825ee
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-03-24 09:50:15 -0300

    Config SMA

--- SMA_en_grafico.py
+++ SMA_en_grafico.py
@@ -8,12 +8,18 @@ df = pd.read_csv('price.csv')
 df['CloseMax'] = df['Close'] + 1000
 #print(df.head())
 
+# Calculo SMA
+sma_20=int(20)
+df['sma_price'] = df['Close'].rolling(window=sma_20).mean()
+# print(sma_price.tail())
+
+
 # Plot de df
 fig, ax = plt.subplots()
-ax.plot(df['Date'], df['Close'], df['CloseMax'], marker='*')
+ax.plot(df['Date'], df['Close'], df['sma_price'], marker='*')
 ax.set_title('Precios', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
 ax.set_ylabel("Precios")
-df.plot(ax=ax)
+df.plot(ax=ax)  #cuadro de referencias de lineas
 
 #size dibujo
 anchoDibujo = int(df['Close'].size)
@@ -23,6 +29,9 @@ fig.set_figwidth(anchoDibujo)
 #Lineas
 plt.hlines(y=4500,  xmin=0, xmax=anchoDibujo, colors='green', linestyles='solid')
 plt.hlines(y=3000,  xmin=0, xmax=anchoDibujo, colors='red', linestyles='solid')
+
+#Rotacion de labels
+plt.xticks(rotation=90)
 
 plt.grid()
 plt.show()
