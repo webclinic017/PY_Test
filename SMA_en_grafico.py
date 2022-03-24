d95a9eac77faa66f992eab9930c7efe8a922a569
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-03-24 09:54:16 -0300

    Config SMA

--- SMA_en_grafico.py
+++ SMA_en_grafico.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 #Lectura de csv
 df = pd.read_csv('price.csv')
 
-df['CloseMax'] = df['Close'] + 1000
+#df['CloseMax'] = df['Close'] + 1000
 #print(df.head())
 
 # Calculo SMA
 sma_20=int(20)
 df['sma_price'] = df['Close'].rolling(window=sma_20).mean()
-# print(sma_price.tail())
-
+# print(df.head())   #check primeros values
+# print(sma_price.tail())  #check ultimos values
 
 # Plot de df
 fig, ax = plt.subplots()
 ax.plot(df['Date'], df['Close'], df['sma_price'], marker='*')
 ax.set_title('Precios', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
 ax.set_ylabel("Precios")
-df.plot(ax=ax)  #cuadro de referencias de lineas
+df.plot(ax=ax)  #referencias de lineas
 
 #size dibujo
 anchoDibujo = int(df['Close'].size)
@@ -27,7 +27,7 @@ fig.set_figheight(10)
 fig.set_figwidth(anchoDibujo)
 
 #Lineas
-plt.hlines(y=4500,  xmin=0, xmax=anchoDibujo, colors='green', linestyles='solid')
+plt.hlines(y=4000,  xmin=0, xmax=anchoDibujo, colors='green', linestyles='solid')
 plt.hlines(y=3000,  xmin=0, xmax=anchoDibujo, colors='red', linestyles='solid')
 
 #Rotacion de labels
