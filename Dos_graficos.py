dcbd1c2a00c7115994c47a2d36334f9d2e56b868
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-03-25 15:38:54 -0300

    Config 2 graficos

--- Dos_graficos.py
+++ Dos_graficos.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('price.csv')
 
 # Calculo SMA
+size_sma_max=int(20)
 sma_20=int(20)
 df['SMA_20_price'] = df['Close'].rolling(window=sma_20).mean()
 
@@ -22,7 +23,7 @@ ax1.plot(df['Date'], df['Close'], marker='*', label='Price Cripto', linestyle='s
 ax1.plot(df['Date'], df['SMA_20_price'], marker='*', label='SMA 20', linestyle='solid', linewidth=3, color='#FF5A17')
 
 ax2 = plt.subplot(212, sharex=ax1) #filas: 2, Columnas: 1, Ubicacion 2 fila)
-ax2.plot(df['SMA_10_price'], marker='.', label='SMA 10', linestyle='solid', linewidth=3, color='#FF5A17')
+ax2.plot(df['SMA_10_price'], marker='.', label='SMA 10', linestyle='solid', linewidth=3, color='green')
 
 ax1.set_title('Precios', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
 ax1.set_ylabel("Precios")
@@ -37,12 +38,19 @@ fig.set_figheight(10)
 fig.set_figwidth(anchoDibujo)
 
 #Hlines solo para grafico ax1
-ax1.hlines(y=4000,  xmin=0, xmax=anchoDibujo, colors='green', linestyle='dotted', linewidth=2)
-ax1.hlines(y=3000,  xmin=0, xmax=anchoDibujo, colors='red', linestyle='dotted', linewidth=2)
+ax1.hlines(y=3400,  xmin=0, xmax=anchoDibujo, colors='green', linestyle='dotted', linewidth=2)
+ax1.hlines(y=2500,  xmin=0, xmax=anchoDibujo, colors='red', linestyle='dotted', linewidth=2)
+
+#Ocultar "escala x" en ax1
+ax1.xaxis.set_visible(False)
 
 #Rotacion de labels
 plt.xticks(rotation=90)
 
+#Mostrar rango de items en grafica
+plt.xlim(size_sma_max,)
 
-plt.grid()
+#Grilla
+ax1.grid(),ax2.grid()
+
 plt.show()
