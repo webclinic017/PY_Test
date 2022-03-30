14ed0bd1d3668d60f05652c1791721cf923adca0
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-03-30 15:57:37 -0300

    Renko Data

--- Renko_basic.py
+++ Renko_basic.py
@@ -1,19 +1,10 @@
 
 import pandas as pd
-import pandas_ta.overlap as ta_overlap
-
 from stocktrends import indicators
-
-# df = pd.read_csv('HDFCLIFE.csv')
-# df.columns = [i.lower() for i in df.columns]
 
 df = pd.read_csv('price.csv')
 df.columns = [i.lower() for i in df.columns]
-
-#df['HL2'] = ta_overlap.hl2(high=df['High'], low=df['Low'])
-#print("\n *** HL2: \n", df['HL2'])
-
-print("\n *** df: \n", df)
+print("*** df: \n", df)
 
 renko = indicators.Renko(df)
 renko.brick_size = 100
