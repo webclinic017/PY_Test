7c1cd1b33b874b25fabfb98bba0ae6191ee77132
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-04-01 11:54:09 -0300

    Renko Plot

---
+++ Basic/RenkoPlot.py
@@ -0,0 +1,27 @@
+
+# import pandas as pd
+# from pandas_datareader import data as pdr
+# import mplfinance as fplt
+#
+# #Assign variables ticker, sdate, size which is the brick size
+# ticker = "AMD"
+# sdate = "2020-01-01"
+# size = 10
+#
+# df = pdr.get_data_yahoo(ticker, start = sdate)
+# print(df)
+#
+# fplt.plot(df,type='renko',renko_params=dict(brick_size=size),style='yahoo',figsize =(18,7), title = "RENKO CHART FOR {0}".format(ticker))
+#
+
+
+
+
+
+
+
+
+
+
+
+
