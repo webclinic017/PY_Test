321eb53d7563e1968edbf89a6f208d16e897d933
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-04-03 09:24:19 -0300

    Candle plot

--- Basic/Candle_plot.py
+++ Basic/Candle_plot.py
@@ -2,43 +2,71 @@
 # Candles usando matplotlib.pyplot:  https://www.statology.org/matplotlib-python-candlestick-chart/
 # Candles usando mplfinance: https://github.com/matplotlib/mplfinance#usage
 
+# import pandas as pd
+# import matplotlib.pyplot as plt
+#
+# #Lectura de csv
+# df = pd.read_csv('price.csv')
+# df['HL2'] = (df['High']+df['Low'])/2
+#
+# #display DataFrame
+# print(df)
+#
+# #create figure
+# plt.figure()
+#
+# #define width of candlestick elements
+# width = .4
+# width2 = .05
+#
+# #define up and down prices
+# up = df[df.Close>=df.Open]
+# down = df[df.Close<df.Open]
+#
+# #define colors to use
+# col1 = 'green'
+# col2 = 'red'
+#
+# #plot up prices
+# plt.bar(up.index,up.Close-up.Open,width,bottom=up.Open,color=col1)
+# plt.bar(up.index,up.High-up.Close,width2,bottom=up.Close,color=col1)
+# plt.bar(up.index,up.Low-up.Open,width2,bottom=up.Open,color=col1)
+#
+# #plot down prices
+# plt.bar(down.index,down.Close-down.Open,width,bottom=down.Open,color=col2)
+# plt.bar(down.index,down.High-down.Open,width2,bottom=down.Open,color=col2)
+# plt.bar(down.index,down.Low-down.Close,width2,bottom=down.Close,color=col2)
+#
+# #rotate x-axis tick labels
+# plt.xticks(rotation=45, ha='right')
+#
+# #display candlestick chart
+# plt.show()
+
 import pandas as pd
-import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 #Lectura de csv
 df = pd.read_csv('price.csv')
 df['HL2'] = (df['High']+df['Low'])/2
 
-#display DataFrame
-print(df)
+df.index = pd.DatetimeIndex(df['Date'])
 
-#create figure
-plt.figure()
+# Slice de dataframe
+# start_date = '2016-02-27'
+# end_date   = '2021-02-27'
+# df = df.loc[start_date:end_date]
 
-#define width of candlestick elements
-width = .4
-width2 = .05
+#mpf.plot(df,type='renko')
+mpf.plot(df,type='candle')
 
-#define up and down prices
-up = df[df.Close>=df.Open]
-down = df[df.Close<df.Open]
 
-#define colors to use
-col1 = 'green'
-col2 = 'red'
 
-#plot up prices
-plt.bar(up.index,up.Close-up.Open,width,bottom=up.Open,color=col1)
-plt.bar(up.index,up.High-up.Close,width2,bottom=up.Close,color=col1)
-plt.bar(up.index,up.Low-up.Open,width2,bottom=up.Open,color=col1)
 
-#plot down prices
-plt.bar(down.index,down.Close-down.Open,width,bottom=down.Open,color=col2)
-plt.bar(down.index,down.High-down.Open,width2,bottom=down.Open,color=col2)
-plt.bar(down.index,down.Low-down.Close,width2,bottom=down.Close,color=col2)
 
-#rotate x-axis tick labels
-plt.xticks(rotation=45, ha='right')
 
-#display candlestick chart
-plt.show()
+
+
+
+
+
