7df5a2f8228b5c07047f9146ccd266e9f203ca2b
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-04-03 11:44:01 -0300

    Candle plot

--- Candle_en_Grafico.py
+++ Candle_en_Grafico.py
@@ -33,8 +33,8 @@ ax2 = fig.add_subplot(2,1,2, sharex=ax1, style='yahoo')
 
 #ax1.plot(df['Date'], df['Close'], marker='*', label='Price Cripto', linestyle='solid', linewidth=3, color='#0091FF')
 
-ap = [ mpf.make_addplot(df[['RSI','SMA_RSI']],type='line', ax=ax2, ylabel='RSI'),
-       mpf.make_addplot(df[['SMA_Rapida','SMA_Lenta']], type='line', ax=ax1, ylabel='SMAs')
+ap = [ mpf.make_addplot(df[['RSI','SMA_RSI']],type='line', ax=ax2),
+       mpf.make_addplot(df[['SMA_Rapida','SMA_Lenta']], type='line', ax=ax1)
      ]
 
 
@@ -42,17 +42,16 @@ ap = [ mpf.make_addplot(df[['RSI','SMA_RSI']],type='line', ax=ax2, ylabel='RSI')
 #Hlines grafico
 anchoDibujo = int(df['Close'].size)
 ax2.hlines(y=30,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
-ax2.hlines(y=50,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
+ax2.hlines(y=50,  xmin=0, xmax=anchoDibujo, colors='grey', linestyle='dotted', linewidth=2)
 ax2.hlines(y=70,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
 
 # Titulos
-ax1.set_title('Precios**', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
-ax1.set_ylabel("Precios---")
-
+ax1.set_title('Precios', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
+ax2.set_title('RSI', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
 
 #Referencias de lineas
-ax1.legend(loc='best')
-ax2.legend(loc='best')
+# ax1.legend(loc='best')
+# ax2.legend(loc='best')
 
 #size dibujo
 fig.set_figheight(10)
@@ -60,13 +59,14 @@ fig.set_figwidth(anchoDibujo)
 
 #Ocultar "escala x" en ax1
 ax1.xaxis.set_visible(False)
+ax1.yaxis.set_visible(True)
 
 #Grilla
 ax1.grid(),ax2.grid()
 
 
 # Plot grafico
-mpf.plot(df, type='candle', ax=ax1, addplot=ap)
+mpf.plot(df, type='candle', ax=ax1, addplot=ap, xrotation=90)
 mpf.show()
 
 
