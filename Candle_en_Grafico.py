ba19206f1e7024db02ef05af106f119bccb55bf3
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-04-03 11:35:17 -0300

    Candle plot

--- Candle_en_Grafico.py
+++ Candle_en_Grafico.py
@@ -28,56 +28,45 @@ df['SMA_RSI'] = ta_overlap.sma(close=df['RSI'], length=rsi_periodo, talib=True)
 
 # Plot de DataFrames
 fig = mpf.figure()
-ax1 = fig.add_subplot(2,1,1,style='yahoo')
+ax1 = fig.add_subplot(2,1,1, style='yahoo')
+ax2 = fig.add_subplot(2,1,2, sharex=ax1, style='yahoo')
 
-#mpf.plot(df,type='candle',ax=ax1,axtitle='Price Cripto',xrotation=15)
 #ax1.plot(df['Date'], df['Close'], marker='*', label='Price Cripto', linestyle='solid', linewidth=3, color='#0091FF')
 
-ap = mpf.make_addplot(df[['SMA_Rapida','SMA_Lenta']],ax=ax1,ylabel='SMAs')
-mpf.plot(df, type='candle', ax=ax1, addplot=ap, xrotation=10, axtitle='Price Cripto')
+ap = [ mpf.make_addplot(df[['RSI','SMA_RSI']],type='line', ax=ax2, ylabel='RSI'),
+       mpf.make_addplot(df[['SMA_Rapida','SMA_Lenta']], type='line', ax=ax1, ylabel='SMAs')
+     ]
 
+
+# Segundo grafico
+#Hlines grafico
+anchoDibujo = int(df['Close'].size)
+ax2.hlines(y=30,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
+ax2.hlines(y=50,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
+ax2.hlines(y=70,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
+
+# Titulos
+ax1.set_title('Precios**', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
+ax1.set_ylabel("Precios---")
+
+
+#Referencias de lineas
+ax1.legend(loc='best')
+ax2.legend(loc='best')
+
+#size dibujo
+fig.set_figheight(10)
+fig.set_figwidth(anchoDibujo)
+
+#Ocultar "escala x" en ax1
+ax1.xaxis.set_visible(False)
+
+#Grilla
+ax1.grid(),ax2.grid()
+
+
+# Plot grafico
+mpf.plot(df, type='candle', ax=ax1, addplot=ap)
 mpf.show()
-
-
-# # Plot SMAs sobre candles
-# ax1.plot(df['Date'], df['SMA_Rapida'], marker='*', label='SMA Rapida', linestyle='solid', linewidth=2, color='green')
-# ax1.plot(df['Date'], df['SMA_Lenta'], marker='*', label='SMA Lenta', linestyle='solid', linewidth=2, color='#FF5A17')
-
-#
-# # Segundo grafico
-# ax2 = plt.subplot(212, sharex=ax1) #filas: 2, Columnas: 1, Ubicacion 2 fila)
-# ax2.plot(df['RSI'], marker='.', label='RSI', linestyle='solid', linewidth=3, color='green')
-# ax2.plot(df['SMA_RSI'], marker='.', label='RSI', linestyle='solid', linewidth=3, color='red')
-#
-# #Hlines grafico
-# anchoDibujo = int(df['Close'].size)
-# ax2.hlines(y=30,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
-# ax2.hlines(y=50,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
-# ax2.hlines(y=70,  xmin=0, xmax=anchoDibujo, colors='orange', linestyle='dotted', linewidth=2)
-#
-# ax1.set_title('Precios', loc="left", fontdict = {'fontsize':8, 'fontweight':'bold', 'color':'tab:blue'})
-# ax1.set_ylabel("Precios")
-#
-# #Referencias de lineas
-# ax1.legend(loc='best')
-# ax2.legend(loc='best')
-#
-# #size dibujo
-# fig.set_figheight(10)
-# fig.set_figwidth(anchoDibujo)
-#
-# #Ocultar "escala x" en ax1
-# ax1.xaxis.set_visible(False)
-#
-# #Rotacion de labels
-# plt.xticks(rotation=90)
-#
-# #Mostrar rango de items en grafica
-# plt.xlim(size_sma_min,)
-#
-# #Grilla
-# ax1.grid(),ax2.grid()
-#
-# plt.show()
 
 
