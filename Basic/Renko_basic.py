18ccf2f7363cdacacb608fb285235460ec6f3a8c
Julio Martin <juliofabianmartin@yahoo.com.ar>
2022-04-05 15:44:51 -0300

    Usando RK_data

--- Basic/Renko_basic.py
+++ Basic/Renko_basic.py
@@ -1,16 +1,16 @@
 #
-# import pandas as pd
-# from stocktrends import indicators
-#
-# df = pd.read_csv('price.csv')
-# df.columns = [i.lower() for i in df.columns]
-# print("*** df: \n", df)
-#
-# renko = indicators.Renko(df)
-# renko.brick_size = 100
-# renko.chart_type = indicators.Renko.PERIOD_CLOSE
-# data_RK = renko.get_ohlc_data()
-# print('\n **** Renko', data_RK)
+import pandas as pd
+from stocktrends import indicators
+
+df = pd.read_csv('price.csv')
+df.columns = [i.lower() for i in df.columns]
+print("*** df: \n", df)
+
+renko = indicators.Renko(df)
+renko.brick_size = 50
+renko.chart_type = indicators.Renko.PERIOD_CLOSE
+data_RK = renko.get_ohlc_data()
+print('\n **** Renko', data_RK)
 
 # # Ejemplo velas
 # import finplot as fplt
